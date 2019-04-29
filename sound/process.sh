f4d6106fbc70f07d547d8a3db775752aca17b266
nudon <hessryan96@gmail.com>
2019-04-29 00:17:56 -0700

    added shell script for converting things to .wav format

---
+++ sound/process.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+space=" "
+empty=""
+hyphen="-"
+
+convertAll() {
+    ext=$1
+    match=./*."$ext"
+    for a in $match; do
+	#for some reason file name found is the reg-exp search term
+	if  [ "$a" != "$match" ]; then
+	    z="${a//[[:punct:]]/$empty}"
+	    b="${z[@]/%$ext/.wav}"
+	    c="${b//$space/$empty}"
+	    d="${c//[[:digit:]]/}"
+	    echo "converting $a to a wav and deleting original file" 
+	    ffmpeg -i "$a" -f wav "$d"
+	    rm "$a"
+	fi
+    done
+
+}
+
+convertAll "mp3"
+convertAll "flac"
